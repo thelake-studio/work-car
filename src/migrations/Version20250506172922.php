13fbdbd7084f6dc5e00a9a1b7a1a6a5ad0c8f962
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2025-05-06 17:33:57 +0000

    Generate migrations for PlannedTrip and RecordedTrip entities

---
+++ src/migrations/Version20250506172922.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250506172922 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE planned_trip (id INT AUTO_INCREMENT NOT NULL, planned_date DATE NOT NULL, start_time TIME NOT NULL, end_time TIME NOT NULL, state VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE recorded_trip (id INT AUTO_INCREMENT NOT NULL, distance DOUBLE PRECISION NOT NULL, duration DOUBLE PRECISION NOT NULL, fuel_consumption DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE planned_trip
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE recorded_trip
+        SQL);
+    }
+}
