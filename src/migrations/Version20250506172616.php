7615103e99205678822911cb441ca7a3ac22e65a
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2025-05-06 17:33:05 +0000

    Generate migrations for GroupLocation and GroupSchedule entities

---
+++ src/migrations/Version20250506172616.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250506172616 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE group_location (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, latitude NUMERIC(10, 7) NOT NULL, longitude NUMERIC(10, 7) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE group_schedule (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, week_day VARCHAR(255) NOT NULL, start_time TIME NOT NULL, end_time TIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE group_location
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE group_schedule
+        SQL);
+    }
+}
