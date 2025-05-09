89e5a4e9258d861ee62f08d0a048c63637823875
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2025-05-09 08:54:46 +0000

    Add unique constraints to email and phoneNumber fields in User entity

--- src/src/Entity/User.php
+++ src/src/Entity/User.php
@@ -6,8 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'Este email ya está registrado.')]
+#[UniqueEntity(fields: ['phoneNumber'], message: 'Este número de teléfono ya está registrado.')]
 class User
 {
     #[ORM\Id]
