87242e8218fe9d552a2c1a54d5f40b4220a1c544
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2025-05-09 09:12:55 +0000

    Update name field for better user experience in User form

--- src/src/Form/UserForm.php
+++ src/src/Form/UserForm.php
@@ -2,20 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Group;
-use App\Entity\PlannedTrip;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nombre *',
+                'attr' => [
+                    'placeholder' => 'Ej: José Luis',
+                    'autocomplete' => 'given-name'
+                ],
+                'help' => 'Mínimo 2 caracteres, máximo 50'
+            ])
             ->add('surname')
             ->add('phoneNumber')
             ->add('email')
